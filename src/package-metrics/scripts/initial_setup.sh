5329c016cbcb68ad273874e62253d44a8b941a6c
dokugaron <82846166+dokugaron@users.noreply.github.com>
2023-04-07 13:31:05 -0400

    delete unused files

--- src/package-metrics/scripts/initial_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git -c advice.detachedHead=false checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-) && \. "$NVM_DIR/nvm.sh"
