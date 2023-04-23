22f1961a45a134685d10cc491c256baf0702badc
dokugaron <82846166+dokugaron@users.noreply.github.com>
2023-04-23 18:01:46 -0400

    get url from database

--- src/routes/package/rate.ts
+++ src/routes/package/rate.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { process_urls, calc_final_result, OutputObject }  from '../../package-metrics/src/index';
 import { Request, Response } from 'express';
+import { prisma } from '../../prisma';
 
 const router = express.Router();
 
@@ -9,9 +10,16 @@ router.get('/:id/rate', async (req: Request, res: Response) => {
     //for now it's set up to get the url from a file.
     //await process_urls(filename, calc_final_result);
     //TODO: add 400 and 404 responses
+
+    const packageRecord = await prisma.package.findUnique({where: { id: req.params.id }});
+
+    if (!packageRecord) {
+        res.status(404).end();
+    }
+
     try {
         //var url_vals:string[] = await get_file_lines('../sample_url_file copy.txt');
-        const rating:OutputObject[] = await process_urls(["https://github.com/nullivex/nodist"], calc_final_result);
+        const rating:OutputObject[] = await process_urls([packageRecord.url], calc_final_result);
         res.status(200).json(rating[0]);
     } catch {
         res.status(500).end();
