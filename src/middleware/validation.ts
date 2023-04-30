0a466e96d7a32709c11643fe2b38d7a2559a1b8b
dsrasheed <daniyaalrasheed@gmail.com>
2023-04-30 19:09:40 -0400

    fix crashes for very large offsets to POST /packages

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 
+function isValidNumber(value: number) {
+    return value != undefined && !isNaN(value) && isFinite(value);
+}
+
 function ValidateParamIsNumberElse(paramName: string, statusCode: number)  {
     return (req: Request, res: Response, next: NextFunction) => {
         const num = parseInt(req.params[paramName]);
-        if (!num || isNaN(num) || !isFinite(num)) {
+        if (!isValidNumber(num)) {
             logger.log('info', `${paramName} is not a valid number`);
             res.status(statusCode).end();
             return;
@@ -13,5 +17,6 @@ function ValidateParamIsNumberElse(paramName: string, statusCode: number)  {
 }
 
 export {
+    isValidNumber,
     ValidateParamIsNumberElse
 }
